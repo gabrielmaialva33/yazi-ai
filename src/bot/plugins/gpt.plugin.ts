066c15feb1dc5e6904a29f4e41af806970c7fa11
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-03-30 13:57:24 -0300

    feat: improvements code

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -2,11 +2,17 @@ import * as fs from 'fs'
 import * as process from 'process'
 import jimp from 'jimp'
 
-import { Configuration, OpenAIApi } from 'openai'
+import {
+  ChatCompletionRequestMessage,
+  ChatCompletionRequestMessageRoleEnum,
+  Configuration,
+  OpenAIApi,
+} from 'openai'
 
 import { Env } from '@/env'
 import { StringUtils } from '@/helpers/string.utils'
 import { Logger } from '@/helpers/logger.utils'
+import * as console from 'console'
 
 class OpenAI extends OpenAIApi {
   constructor() {
@@ -60,6 +66,79 @@ class OpenAI extends OpenAIApi {
     Logger.info(`Variating image: ${path}.png`, 'IA')
 
     return this.createImageVariation(fs.createReadStream(`${path}.png`) as any, 1, '512x512', 'url')
+  }
+
+  public async chat(text: string, username: string) {
+    const system = fs.readFileSync(process.cwd() + '/tmp/system.gpt.txt', 'utf8')
+    const history = fs.readFileSync(process.cwd() + '/tmp/history.gpt.txt', 'utf8')
+
+    // split the text in lines
+    const lines_history: string[] = history.split('\n') as unknown as string[]
+    lines_history.pop()
+    console.log({ lines_history })
+
+    const messages_history = lines_history.map((line) => {
+      const [user, message] = line.split(']:') || line.split('):')
+
+      const reply_to_user = user.includes('(') ? user.split('(')[1].split(')')[0] : user
+      const new_user = user.includes('(') ? user.split('(')[0] : user
+
+      const modified_message = reply_to_user
+        ? message.slice(0, 1) + `(reply: ${reply_to_user}) ` + message.slice(1)
+        : message
+
+      return {
+        role:
+          user.includes('Yazi AI') && !user.includes('(')
+            ? ChatCompletionRequestMessageRoleEnum.Assistant
+            : ChatCompletionRequestMessageRoleEnum.User,
+        name: new_user ? new_user : user,
+        content: modified_message,
+      }
+    })
+
+    const line_text = text.split(']:')
+    const [user, message] = line_text
+    const reply_to_user = user.includes('(') ? user.split('(')[1].split(')')[0] : user
+    const new_user = user.includes('(') ? user.split('(')[0] : user
+
+    console.log({ reply_to_user, new_user, message })
+
+    const modified_message = reply_to_user
+      ? message.slice(0, 1) + `(reply: ${reply_to_user}) ` + message.slice(1)
+      : message
+
+    const messages_text = {
+      role:
+        user.includes('Yazi AI') && !user.includes('(')
+          ? ChatCompletionRequestMessageRoleEnum.Assistant
+          : ChatCompletionRequestMessageRoleEnum.User,
+      name: new_user ? new_user : user,
+      content: modified_message.split('\n')[0],
+    }
+
+    messages_history.push(messages_text)
+
+    const messages: Array<ChatCompletionRequestMessage> = [
+      {
+        role: ChatCompletionRequestMessageRoleEnum.System,
+        content: system,
+      },
+      ...messages_history,
+    ]
+
+    console.log({ messages })
+
+    return this.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      stop: ['|'],
+      max_tokens: 500,
+      temperature: 1,
+      presence_penalty: Math.random() * (0.3 - 0.1) + 0.1,
+      frequency_penalty: Math.random() * (1.0 - 0.5) + 0.5,
+      messages: messages,
+      n: 1,
+    })
   }
 }
 
